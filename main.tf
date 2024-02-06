efe3b4d2f41554bbe8f99ced8a50fb9ab3f209a1
Boris Popov <popov.boris@nebius.com>
2024-02-06 14:18:06 +0100

    ARCHVTEAMS-64 tasks
    ..

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ locals {
   cluster_nodes = [for node_num in range(1, var.cluster_nodes_count + 1): "node-${node_num}"]
 }
 
-resource "random_password" "mysql" {
-  length           = 16
-  special          = false
-}
 
 
 resource "nebius_compute_gpu_cluster" "slurm-cluster" {
@@ -14,23 +10,59 @@ resource "nebius_compute_gpu_cluster" "slurm-cluster" {
   zone               = var.region
 }
 
-resource "nebius_iam_service_account" "saccount" {
-  name        = "sa-slurm"
-  description = "Service account for slurm cluster"
-  folder_id   = var.folder_id
+
+resource "tls_private_key" "master_key" {
+  algorithm = "RSA"
 }
 
-resource "nebius_resourcemanager_folder_iam_member" "monitoring-editor" {
-  folder_id   = var.folder_id
-  role        = "monitoring.editor"
-  member      = "serviceAccount:${nebius_iam_service_account.saccount.id}"
+
+resource "nebius_compute_instance" "master" {
+  depends_on = [nebius_mdb_mysql_user.slurmuser, nebius_compute_instance.slurm-node]
+  name           = "node-master"
+  platform_id    = "standard-v2"
+  hostname       = "node-master"
+  zone           = var.region
+  
+  resources {
+    cores  = "16"
+    memory = "32"
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.ib_image_id
+      size = "1024"
+      type = "network-ssd"
+    }
+  }
+
+  network_interface {
+    subnet_id = nebius_vpc_subnet.slurm-subnet.id
+    #nat_ip_address  = nebius_vpc_address.node-master-ip.external_ipv4_address[0].address
+    nat = true
+  }
+
+  metadata = {
+    user-data = templatefile(
+      "${path.module}/files/cloud-config-master.yaml.tftpl", {
+        ENROOT_VERSION = "3.4.1"
+        is_master      = "1"
+        is_mysql       = var.mysql_jobs_backend
+        sshkey         = var.sshkey
+        cluster_nodes_count = var.cluster_nodes_count
+        hostname            = var.mysql_jobs_backend ? nebius_mdb_mysql_cluster.slurm-mysql-cluster[0].host[0].fqdn : ""
+        password            = random_password.mysql.result
+        is_mysql            = var.mysql_jobs_backend
+        master_pubkey  = trimspace(tls_private_key.master_key.public_key_openssh)
+        master_privkey = split("\n", tls_private_key.master_key.private_key_openssh)
+
+      })
+  }
+
 }
 
-resource "nebius_resourcemanager_folder_iam_member" "container-editor" {
-  folder_id   = var.folder_id
-  role        = "container-registry.editor"
-  member      = "serviceAccount:${nebius_iam_service_account.saccount.id}"
-}
+
+
 
 resource "nebius_compute_instance" "slurm-node" {
   for_each       = toset( local.cluster_nodes )
@@ -47,6 +79,11 @@ resource "nebius_compute_instance" "slurm-node" {
     gpus   = "8"
   }
 
+  # resources {
+  #   cores  = "16"
+  #   memory = "32"
+  # }
+
   boot_disk {
     initialize_params {
       image_id = var.ib_image_id
@@ -56,113 +93,26 @@ resource "nebius_compute_instance" "slurm-node" {
   }
 
   network_interface {
-    subnet_id = nebius_vpc_subnet.subnet-1.id
-    nat       = true
+    subnet_id = nebius_vpc_subnet.slurm-subnet.id
+    nat       = false
   }
 
   metadata = {
     install-unified-agent = 1
     user-data = templatefile(
-      "${path.module}/files/cloud-config.yaml.tftpl", {
+      "${path.module}/files/cloud-config-worker.yaml.tftpl", {
         ENROOT_VERSION = "3.4.1"
         is_master      = "0"
         is_mysql       = var.mysql_jobs_backend
         sshkey         = var.sshkey
+        cluster_nodes_count = var.cluster_nodes_count
+        hostname            = var.mysql_jobs_backend ? nebius_mdb_mysql_cluster.slurm-mysql-cluster[0].host[0].fqdn : ""
+        password            = random_password.mysql.result
+        is_mysql            = var.mysql_jobs_backend
+        master_pubkey  = trimspace(tls_private_key.master_key.public_key_openssh)
       })
   }
 }
 
 
-resource "nebius_compute_instance" "master" {
-  name           = "node-master"
-  platform_id    = "standard-v2"
-  hostname       = "node-master"
-  zone           = var.region
-  
-  resources {
-    cores  = "16"
-    memory = "32"
-  }
 
-  boot_disk {
-    initialize_params {
-      image_id = var.ib_image_id
-      size = "100"
-      type = "network-ssd"
-    }
-  }
-
-  network_interface {
-    subnet_id = nebius_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    user-data = templatefile(
-      "${path.module}/files/cloud-config.yaml.tftpl", {
-        ENROOT_VERSION = "3.4.1"
-        is_master      = "1"
-        is_mysql       = var.mysql_jobs_backend
-        sshkey         = var.sshkey
-      })
-  }
-}
-
-resource "nebius_mdb_mysql_cluster" "slurm-mysql-cluster" {
-  count               = var.mysql_jobs_backend ? 1 : 0
-  name                = "nebius-mysql-cluster"
-  environment         = "PRODUCTION"
-  network_id          = nebius_vpc_network.slurm-network.id
-  version             = "8.0" 
-
-  resources {
-    resource_preset_id = "s3-c8-m32"
-    disk_type_id       = "network-ssd"
-    disk_size          = "200"
-  }
-  mysql_config = {
-    innodb_lock_wait_timeout = 900
-  }
-
-  host {
-    zone             = var.region
-    subnet_id        = nebius_vpc_subnet.subnet-1.id
-    assign_public_ip = false
-    priority  = 90
-  }
-
-  host {
-    zone             = var.region
-    subnet_id        = nebius_vpc_subnet.subnet-1.id
-    assign_public_ip = false
-  }
-}
-
-resource "nebius_mdb_mysql_database" "slurm-db" {
-  count        = var.mysql_jobs_backend ? 1 : 0
-  cluster_id   = nebius_mdb_mysql_cluster.slurm-mysql-cluster[0].id
-  name         = "slurm-db"
-}
-
-resource "nebius_mdb_mysql_user" "slurmuser" {
-  count      = var.mysql_jobs_backend ? 1 : 0
-  cluster_id =  nebius_mdb_mysql_cluster.slurm-mysql-cluster[0].id
-  name       = "slurm"
-  password   = random_password.mysql.result
-  permission {
-    database_name = nebius_mdb_mysql_database.slurm-db[0].name
-    roles         = ["ALL"]
-  }
-}
-
-
-resource "nebius_vpc_network" "slurm-network" {
-  name = "slurm-network"
-}
-
-resource "nebius_vpc_subnet" "subnet-1" {
-  name           = "subnet-1"
-  zone           = "eu-north1-c"
-  v4_cidr_blocks = ["192.168.10.0/24"]
-  network_id     = nebius_vpc_network.slurm-network.id
-}
